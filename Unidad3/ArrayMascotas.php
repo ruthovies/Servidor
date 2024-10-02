5312b1890b9038011eaae024a711f75724b5099e
RuthOvies <ruthpa07@educastur.es>
2024-10-02 13:39:30 +0200

    commit5

---
+++ Unidad3/ArrayMascotas.php
@@ -0,0 +1,8 @@
+<?php
+ $pets = array('perro' => "Yunito",
+ 'gato' => "Wilson",
+ 'canario' => "Piolin",
+ 'tortuga' => "Pepe");
+ foreach($pets as $item => $description)
+ echo "$item: $description<br>";
+?> 
