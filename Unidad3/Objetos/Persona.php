9593bb7f78e20a6bacbe5d529b426e8c3d066347
RuthOvies <ruthpa07@educastur.es>
2024-10-10 14:05:50 +0200

    obj

--- Unidad3/Objetos/Persona.php
+++ Unidad3/Objetos/Persona.php
@@ -3,5 +3,14 @@ class Persona {
     public $nombre;
     public $apellido;
     public $edad;
+    
+    public function saludar(){
+        return 'Hola, soy ' . $this->nombre . ' ' . $this->apellido . ' y
+        tengo ' . $this->edad . ' años ';
+        }
+
+         public function __construct() {
+        echo 'Se acaba de crear el objeto persona <br>';
+         }
 }
 ?>
--- Unidad3/Objetos/Pruebas.php
+++ Unidad3/Objetos/Pruebas.php
@@ -1,13 +1,19 @@
 <?php
 require_once 'Persona.php';
+
 //Creamos el objeto.
 $persona = new Persona();
+
 //Seteamos las propiedades.
 $persona->nombre = 'Fernando';
 $persona->apellido = 'Gaitan';
 $persona->edad = 26;
+
 //Mostramos el resultado de las propiedades.
 echo 'Nombre: ' . $persona->nombre . '<br />';
 echo 'Apellido: ' . $persona->apellido . '<br />';
 echo 'Edad: ' . $persona->edad . '<br />';
+
+//Llamamos al método.
+echo $persona->saludar();
 ?>
