95437f2e922375593a968d3f5d8314d1ebe0bedb
RuthOvies <ruthpa07@educastur.es>
2024-10-17 14:05:46 +0200

    Actu

--- Unidad4/Formularios/Fichero2.php
+++ Unidad4/Formularios/Fichero2.php
@@ -1,34 +1,35 @@
 <?php
 
 if(isset($POST['nombre'])){
-echo "Nombre" .$POST ['nombre'];
-echo "Apellido" .$POST ['apellido'];
+    echo "Nombre" .$POST ['nombre'];
+    echo "Apellido" .$POST ['apellido'];
+
 }else{
 
- ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario de Datos</title>
-</head>
-<body>
+    ?>
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Formulario de Datos</title>
+    </head>
+    <body>
 
-    <h2>Formulario:</h2>
+        <h2>Formulario:</h2>
 
-    <form action="test.php" method="GET">
-        <label for="nombre">Nombre:</label><br>
-        <input type="text" id="nombre" name="nombre" required><br><br>
+        <form action="test.php" method="GET">
+            <label for="nombre">Nombre:</label><br>
+            <input type="text" id="nombre" name="nombre" required><br><br>
 
-        <label for="apellidos">Apellidos:</label><br>
-        <input type="text" id="apellidos" name="apellidos" required><br><br>
+            <label for="apellidos">Apellidos:</label><br>
+            <input type="text" id="apellidos" name="apellidos" required><br><br>
 
-        <input type="submit" value="Enviar">
-    </form>
+            <input type="submit" value="Enviar">
+        </form>
 
-</body>
-</html>
+    </body>
+    </html>
 <?php
 }
 ?>
