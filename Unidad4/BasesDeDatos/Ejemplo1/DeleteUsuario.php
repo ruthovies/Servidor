d07e1fb96c746bbd94674e610892480841fc8455
RuthOvies <ruthpa07@educastur.es>
2024-11-28 11:54:48 +0100

    bases

--- Unidad4/BasesDeDatos/Ejemplo1/DeleteUsuario.php
+++ Unidad4/BasesDeDatos/Ejemplo1/DeleteUsuario.php
@@ -2,9 +2,8 @@
 
 require_once 'login.php';
 $connection = new mysqli($hn, $un, $pw, $db,3307);//Creamos una conexion añadiendo el puerto 3307, el de nuestra base de datos
- $consulta="INSERT INTO usuarios (Usu, Contra, Rol) VALUES ('Yolanda', 'Yolanda', 'Jugador')"; //Añadimos el usuario
+ $consulta="DELETE FROM usuarios WHERE usu = 'Yolanda'"; //Borramos el usuario
  $result = $connection->query($consulta);//Iniciamos la consulta
 mysqli_close($connection);//Se cierra conexion
-
 
 ?>
